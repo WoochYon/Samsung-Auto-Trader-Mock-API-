94aa2358a565887170c4b86ddc33e3c578571c11
Woochang Jung <137390313+WoochYon@users.noreply.github.com>
2026-06-02 19:09:35 +0900

    Update logger.py

--- logger.py
+++ logger.py
@@ -1,1 +1,14 @@
+import logging
 
+def get_logger(name):
+    logger = logging.getLogger(name)
+    if not logger.handlers:
+        logger.setLevel(logging.INFO)
+        formatter = logging.Formatter('%(asctime)s | %(levelname)s | %(name)s | %(message)s')
+        
+        # Console output
+        ch = logging.StreamHandler()
+        ch.setFormatter(formatter)
+        logger.addHandler(ch)
+        
+    return logger
